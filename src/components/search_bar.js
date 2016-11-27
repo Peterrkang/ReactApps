5be90b96f50b7085eb4dece78cf78956ce480631
Peter Kang <Peterrkang@gmail.com>
2016-11-26 18:15:12 -0800

    add youtubelite project

---
+++ src/components/search_bar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  render() {
+    return (
+      <div className="search-bar">
+        <input
+          value = {this.state.term}
+          onChange = {event => this.onInputChange(event.target.value)} />
+      </div>
+    );
+  };
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+
+  }
+
+
+};
+
+
+
+export default SearchBar;
+
+
+/*
+
+onInputChange(event) {
+  console.log(event.target.value);
+}
+
+
+Value of the input: { this.state.term }
+*/
